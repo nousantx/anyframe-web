52289fe88ff3ebb925de0ab29d2bea385ea511c5
nousantx <nousantx@gmail.com>
2025-01-01 22:48:13 +0700

    feat: add category slugs hehlper

---
+++ src/utils/slugs.ts
@@ -0,0 +1,12 @@
+export function createSlug(text: string): string {
+  return text
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .trim() // Trim - from start and end
+}
+
+export function getComponentBySlug(components: any[], slug: string) {
+  return components.find(component => createSlug(component.name) === slug)
+}
