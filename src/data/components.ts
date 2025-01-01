9f8937cedb4b6ccb19fa9e278f31da811d79f1f3
nousantx <nousantx@gmail.com>
2025-01-01 22:46:19 +0700

    feat: update initial template, update button style

--- src/data/components.ts
+++ src/data/components.ts
@@ -1,17 +1,19 @@
 // src/data/components.ts
-export const componentLibraryData: ComponentLibrary = {
+export const componentData: ComponentLibrary = {
   buttons: {
     title: 'Buttons',
-    description: 'Collection of button components',
+    description: 'Just button, what did you expect?',
     components: [
       {
         name: 'Primary Button',
-        code: '<button class="bg-blue-500 text-white px-4 py-2 rounded">Click me</button>',
+        code: `<button class="[border,outline]-none h-35px px-12px br-8px center bg-blue-500 hover:bg-blue-400 text-fc-neutral-50 tr-time-300ms">
+  Button
+</button>`,
         description: 'Main call-to-action button'
       },
       {
         name: 'Secondary Button',
-        code: '<button class="border-2 border-gray-500 px-4 py-2 rounded">Click me</button>',
+        code: '<button class="[border,outline]-none h-35px px-12px br-8px center bg-neutral-100 hover:bg-neutral-200 text-neutral-950 tr-time-300ms">Button</button>',
         description: 'Secondary action button'
       }
     ]
