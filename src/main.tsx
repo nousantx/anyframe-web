52afbb0df0516a0914847baa8665c3837478042e
nousantx <nousantx@gmail.com>
2025-01-01 22:48:59 +0700

    feat: add theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import { Routes } from '@generouted/react-router'
 import './styles/css/index.css'
+import { ThemeProvider } from './contexts/ThemeContext'
 
-createRoot(document.getElementById('root')!).render(<Routes />)
+createRoot(document.getElementById('root')!).render(
+  <ThemeProvider>
+    <Routes />
+  </ThemeProvider>
+)
