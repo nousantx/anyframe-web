8f4238bfbd70ce475a0e16eb8e30017f99a1575c
nousantx <nousantx@gmail.com>
2025-01-02 22:13:11 +0700

    feat: fixing some layouts

--- src/pages/components/index.tsx
+++ src/pages/components/index.tsx
@@ -4,7 +4,7 @@ import { styler } from '@stylx'
 export default function ComponentsPage() {
   styler()
   return (
-    <div className="p-2rem">
+    <article className="p-2rem pt-6rem">
       <h1 className="text-xl">Component Categories</h1>
 
       <div className="mt-2rem flex gap-1rem flex-wrap">
@@ -12,13 +12,13 @@ export default function ComponentsPage() {
           <Link
             key={key}
             to={`/components/${key}`}
-            className="p-1rem bg-neutral-100 hover:bg-neutral-200 tr-time-300ms br-8px td-none block [flex]-[1_1_200px] text-neutral-900"
+            className="p-1rem [background]-[rgb({neutral-100}_/_var(--this-opa))] tr-time-300ms br-8px td-none block [flex]-[1_1_200px] text-neutral-900 [--this-opa]-0.4 hover:[--this-opa]-0.9"
           >
-            <h2 className="text-xl">{category.title}</h2>
-            <p className="text-gray-600">{category.description}</p>
+            <h2 className="text-lg fs-1.4rem">{category.title}</h2>
+            <p className="pa mt-4px text-gray-600">{category.description}</p>
           </Link>
         ))}
       </div>
-    </div>
+    </article>
   )
 }
