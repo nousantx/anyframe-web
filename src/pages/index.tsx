3c20c617e8031a57b9c9a9bb4a7ce8f9b9f085bd
nousantx <nousantx@gmail.com>
2025-01-02 22:12:30 +0700

    feat: fixing home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,30 @@ import { styler } from '@stylx'
 export default function HomePage() {
   styler()
   return (
-    <article className="p-2rem h-mn-100vh">
+    <article className="p-2rem pt-6rem h-mn-100dvh center">
       <header className="tw-balance ta-center">
-        <h1 className="fs-2.4rem lh-1 fw-600 ls--0.015em">Build Your Own UI Components</h1>
+        <span className="pa fs-12px bg-neutral-700 br-55px px-8px py-4px bg-opacity-0.1">
+          Released V1
+        </span>
+        <h1 className="fs-2.4rem lh-1 fw-600 ls--0.015em mt-1rem">
+          Get Your Own UI Components with{' '}
+          <span
+            className="
+              fw-700
+              [background]-[-webkit-linear-gradient(-45deg,_rgb({blue-600}),_rgb({sky-400}))]
+              [-webkit-background-clip]-text
+              [-webkit-text-fill-color]-transparent"
+          >
+            Unlimited Possibilities
+          </span>
+        </h1>
 
         <p className="mt-1rem text-neutral-800 pa lh-1.4">
           Generate any UI components in your browser. Fully editable directly in your browser!
         </p>
         <div className="center gap-8px">
           <Link
-            to="/components"
+            to="/get-started"
             className="pa mt-2rem td-none center px-14px bg-neutral-950 h-40px br-6px hover:bg-neutral-900 text-neutral-50 pa"
           >
             Get Started
