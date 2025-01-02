04155f3342f5502750d8c9dd443d27fa74015bad
nousantx <nousantx@gmail.com>
2025-01-02 22:12:49 +0700

    feat: update css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { Routes } from '@generouted/react-router'
 import './styles/css/index.css'
+import 'responsive-alt-tui/src/display.css'
 import { ThemeProvider } from './contexts/ThemeContext'
 
 createRoot(document.getElementById('root')!).render(
