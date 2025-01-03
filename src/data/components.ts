8ec005eee925442079abe5d5fdeca31f1cd64baa
nousantx <nousantx@gmail.com>
2025-01-03 20:56:59 +0700

    feat: update components list

--- src/data/components.ts
+++ src/data/components.ts
@@ -47,6 +47,32 @@ export const componentData: ComponentLibrary = {
         name: 'Playground',
         code: '<div class="box-200px center bg-lime-200 br-1rem">Content</div>',
         description: 'Basic accordion implementation'
+      },
+      {
+        name: 'UI Gradient',
+        code: `<div
+  class="family-sans [--gradient]-[conic-gradient(from_45deg,_rgb({amber-500}),_rgb({fuchsia-500}),_rgb({amber-500}))]"
+  data-child="(*): [m,p]-0 [box-sizing]-border-box;"
+>
+  <div class="[--box-size]-250px box-$box-size br-8px center relative [isolation]-isolate [background]-$gradient">
+    <div class="box-$box-size br-8px absolute z--1 [filter]-[blur(30px)] [background]-$gradient"></div>
+    <div
+      class="box-[calc(var(--box-size)_-_10px)] [background]-[linear-gradient(45deg,_rgb({neutral-50}),_rgb({neutral-100}))] text-neutral-900 br-4px p-1.5rem flex flex-col"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+        <path
+          fill="currentColor"
+          d="M6 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3zm10 8l-5 7v-5H8l5-7v5z"
+        />
+      </svg>
+      <header class="mt-auto">
+        <h1 class="text-md ls--0.020em">Hello World!</h1>
+        <p class="text-xs fw-500 text-neutral-400">Hello Lorem ipsum dolor sit.</p>
+      </header>
+    </div>
+  </div>
+</div>`,
+        description: 'Basic accordion implementation'
       }
     ]
   }
